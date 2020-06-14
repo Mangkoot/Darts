2c9389e1c825ea4d8317d872474efbd9545724a6
Magnus Aune <aunern2@gmail.com>
2020-06-14 22:04:42 +0200

    added modal with scoresheet

---
+++ js.js
@@ -0,0 +1,6 @@
+const p1rest = document.getElementById('player1-rest').value;
+const p1score = document.getElementById('player1-score').value;
+
+function p1restScore() {
+    p1rest = p1rest-p1score;
+}
