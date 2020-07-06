47f13acfffc9b7b3b71981224e89c69aa1c0e16a
Magnus Aune <aunern2@gmail.com>
2020-07-06 13:11:46 +0200

    Design changes, a little JavaScript score/rest

--- js.js
+++ js.js
@@ -1,6 +1,31 @@
-const p1rest = document.getElementById('player1-rest').value;
-const p1score = document.getElementById('player1-score').value;
 
-function p1restScore() {
-    p1rest = p1rest-p1score;
-}
+//Ved feil i console - jquery og touchstart
+//    document.addEventListener('touchstart', onTouchStart, {passive: true});
+
+function scoreCountP1() {
+
+    let scoreP1 = $('#player1-score').val();
+    let restP1 = $('#player1-rest').html();
+    event.preventDefault();
+    if ((scoreP1 - restP1) < 0) {
+   $('#player1-rest').html(restP1 - scoreP1);
+   $('#player1-score').val("");
+   $('#player2-score').focus(); 
+  }
+ 
+   else {
+     alert("Du kan ikke skrive mer enn du har igjen (" + restP1 + ")")
+   };
+  }
+
+
+  function scoreCountP2() {
+    let scoreP2 = $('#player2-score').val();
+    let restP2 = $('#player2-rest').html();
+    event.preventDefault()
+   $('#player2-rest').html(restP2 - scoreP2);
+   $('#player2-score').val("");
+   $('#player1-score').focus(); 
+ 
+  }
+
